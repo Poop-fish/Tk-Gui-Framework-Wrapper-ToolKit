b644c76a714be5675f2fb75d73f92e86c239f4b6
GamerTag <justagamert4g@gmail.com>
2025-02-02 20:50:18 -0500

    Update GTG_imports.py

--- GTG_imports.py
+++ GTG_imports.py
@@ -24,4 +24,61 @@ from pygments.lexers import PythonLexer
 
 from pygments.styles import get_style_by_name
 
+from pygments.token import Token 
+
+
+
+
+
+# \\ tk and tkk imports \\ 
+import tkinter as tk
+from tkinter import colorchooser, filedialog, ttk , scrolledtext
+from tkinter import Canvas, Scale, HORIZONTAL
+from tkinter import filedialog 
+from tkinter import PhotoImage 
+from tkinter import simpledialog
+
+# \\ Base imports \\ 
+
+from PIL import Image, ImageDraw  , ImageTk , ImageColor
+
+import math , os , random , inspect 
+
+import colorsys 
+
+from datetime import datetime
+
+from enum import Enum
+
+from pygments import lex
+
+from pygments.lexers import PythonLexer
+
+from pygments.styles import get_style_by_name
+
+from pygments.token import Token 
+
+
+
+# --------------------- 
+
+# TODO :Need to  Organize these imports 
+import time
+import inspect
+import random
+from tkinter import scrolledtext
+from pygments import lex
+from pygments.lexers import PythonLexer
 from pygments.token import Token
+from pygments.style import Style
+from pygments.styles import get_style_by_name 
+
+
+
+
+
+from datetime import datetime
+import tkinter as tk
+from tkinter import messagebox
+from tkinter.simpledialog import askstring
+import calendar
