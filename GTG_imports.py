78b1cec71e2f75e4b183b88fa51f712c822ea9e0
GamerTag <justagamert4g@gmail.com>
2025-02-05 20:34:30 -0500

    Update GTG_imports.py
    organized imports

--- GTG_imports.py
+++ GTG_imports.py
@@ -1,77 +1,46 @@
 # \\ tk and tkk imports \\ 
 import tkinter as tk
-from tkinter import colorchooser, filedialog, ttk , scrolledtext
+
+from tkinter import colorchooser, filedialog, ttk , scrolledtext , messagebox
+
 from tkinter import Canvas, Scale, HORIZONTAL
-from tkinter import filedialog 
+
 from tkinter import PhotoImage 
+
 from tkinter import simpledialog
 
-# \\ Base imports \\ 
-
-from PIL import Image, ImageDraw  , ImageTk , ImageColor
-
-import math , os , random , inspect 
-
-import colorsys 
-
-from datetime import datetime
-
-from enum import Enum
-
-from pygments import lex
-
-from pygments.lexers import PythonLexer
-
-from pygments.styles import get_style_by_name
-
-from pygments.token import Token 
-
-
-
-
-
-# \\ tk and tkk imports \\ 
-import tkinter as tk
-from tkinter import colorchooser, filedialog, ttk , scrolledtext
-from tkinter import Canvas, Scale, HORIZONTAL
-from tkinter import filedialog 
-from tkinter import PhotoImage 
-from tkinter import simpledialog
-
-# \\ Base imports \\ 
-
-from PIL import Image, ImageDraw  , ImageTk , ImageColor
-
-import math , os , random , inspect 
-
-import colorsys 
-
-from datetime import datetime
-
-from enum import Enum
-
-from pygments import lex
-
-from pygments.lexers import PythonLexer
-
-from pygments.styles import get_style_by_name
-
-from pygments.token import Token 
-
-
-
-# --------------------- 
-
-# TODO : Need to Organize these imports and remove doubles 
-import time
-import inspect
-import random
-from pygments import lex
-from pygments.lexers import PythonLexer
-from pygments.token import Token
-from pygments.style import Style
-from pygments.styles import get_style_by_name 
-
-from tkinter import messagebox
 from tkinter.simpledialog import askstring
+
+#! ------------------------------------
+
+
+# \\ Base imports \\ 
+
+from PIL import Image, ImageDraw  , ImageTk , ImageColor
+
+import math , os , random , inspect , time 
+
+import colorsys 
+
 import calendar
+
+import psutil
+
+import GPUtil
+
+
+#! ------------------------------------
+
+from datetime import datetime
+
+from enum import Enum
+
+from pygments import lex
+
+from pygments.lexers import PythonLexer
+
+from pygments.styles import get_style_by_name
+
+from pygments.token import Token 
+
+#! ------------------------------------
